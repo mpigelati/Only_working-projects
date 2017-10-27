328b310d072810a6d980df8898b2c9a23dea691e
mpigelati <saimohansai@gmail.com>
2017-10-27 10:32:40 +0530

    My_adb project

---
+++ My_adb/sample/sample.py
@@ -0,0 +1,21 @@
+import sys
+
+sys.path.append("/home/mohansai/python/Python_adb/M_adb")
+import myadb
+
+
+def main():
+    #print(myadb.hellow())
+    adb = myadb.madb()
+
+    #adb._data_()
+    #adb.run_cmd()
+
+    #adb.adb_devices()
+    #adb.adb_root()
+    adb.meta_info()
+
+
+
+if __name__ == "__main__":
+    main()
